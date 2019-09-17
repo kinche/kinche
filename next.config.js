1c302f40e7fcd48f7cb83d3d234983f4e7f2a839
bukinoshita <bukinoshita@gmail.com>
2019-09-17 17:29:10 -0300

    feat: add env vars

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
+require('dotenv').config()
+
 module.exports = {
-  target: 'serverless'
+  target: 'serverless',
+  env: {
+    COOKIES_NAME: process.env.COOKIES_NAME,
+    COOKIES_DOMAIN: process.env.COOKIES_DOMAIN
+  }
 }
